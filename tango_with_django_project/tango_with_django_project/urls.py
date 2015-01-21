7ce28e5534699718d1896ba213858c8e8c70aae8
Tanner Stults <jst274@cornell.edu>
2015-01-20 23:24:33 -0500

    Changed url to cornell instead of rango

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')),
+    url(r'^cornell/', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
     #url(r'^autocomplete/', include('autocomplete_light.urls')),
