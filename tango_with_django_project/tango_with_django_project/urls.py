5ae3e33479fb640e99b920c034fdcccc1e1bdc76
Tanner Stults <jst274@cornell.edu>
2015-01-21 11:25:20 -0500

    changed registration get_success_url to cornell/add_profile

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls import patterns, url, include
 # Create a new class that redirects the user to the index page, if successful at logging
 class MyRegistrationView(RegistrationView):
     def get_success_url(selfself,request, user):
-        return '/rango/add_profile/'
+        return '/cornell/add_profile/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cornell/', include('rango.urls')),
-    url(r'^rango/', include('rango.urls')),
+    #url(r'^rango/', include('rango.urls')),
     url(r'^$', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
